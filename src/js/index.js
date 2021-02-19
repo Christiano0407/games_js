859e1375fdfce46d25fddaefe3c8de8c5b5a44af
Christiano0407 <placebo0704@gmail.com>
2021-02-18 21:14:30 -0600

    finished

--- src/js/index.js
+++ src/js/index.js
@@ -7,13 +7,17 @@ document.addEventListener(`DOMContentLoaded`, () => {
     let isGoingLeft = false
     let left = 0
     let leftTimeId
+    let isGoingRight = false
+    let rightTimerId
 
     function jump() {
         //Not Jump
         if(isJumping) return
+        prince.classList.add(`character`)
+        prince.classList.remove(`character-sliding`)
         //Intervalo establecido
         let timerUpId = setInterval( function () {
-
+             //Jump-Down
             if(bottom > 250) {
                 clearInterval(timerUpId)
                 let timerDownId = setInterval( function () {
@@ -22,10 +26,12 @@ document.addEventListener(`DOMContentLoaded`, () => {
                         isJumping = false
                     }
                     bottom -=5
+                    bottom = bottom * gravity
                     prince.style.bottom = bottom + `px`
                 }, 20)
             }
 
+            //Jump-Up
             isJumping = true
             bottom += 30
             bottom = bottom * gravity
@@ -36,6 +42,14 @@ document.addEventListener(`DOMContentLoaded`, () => {
     }
 
     function slideLeft () {
+        prince.classList.add(`character`)
+        prince.classList.remove(`charcater-sliding`)
+
+        if(isGoingRight) {
+            clearInterval(rightTimerId)
+            isGoingRight = false
+        }
+
         isGoingLeft = true
         leftTimeId = setInterval(function() {
             console.log(`Going-left`)
@@ -44,12 +58,31 @@ document.addEventListener(`DOMContentLoaded`, () => {
         },20)
     }
 
+    function slideRight () {
+        prince.classList.add(`character-sliding`)
+        prince.classList.remove(`character`)
+
+        if(isGoingLeft) {
+            clearInterval(leftTimeId)
+            isGoingLeft = false
+        }
+
+        isGoingRight = true
+        rightTimerId = setInterval(function () {
+            console.log(`Go to Right`);
+            left +=5
+            prince.style.left = left +`px`
+        },20)
+    }
+
     //Assign function to keycodes
     function control(e) {
-        if(e.keyCode === 38) {
-            jump()
+        if(e.keyCode === 39) {
+            slideRight() // Right arrow on our Keybor - if we press
+        }else if(e.keyCode === 38) {
+            jump() // If we press  the up arrow
         }else if(e.keyCode === 37) {
-            slideLeft() // If we press left
+            slideLeft() // if we press left 
         }
     }
     document.addEventListener(`keydown`, control)
