89dad0a3b32a64b94d1aac72a39210261b41958b
Christiano0407 <placebo0704@gmail.com>
2021-02-21 12:18:22 -0600

    platform_game

--- src/js/app.js
+++ src/js/app.js
@@ -5,6 +5,10 @@ document.addEventListener(`DOMContentLoaded`, () => {
     const doodler = document.createElement(`div`);
     let doodlerLeftSpace = 50
     let platforms = []
+    let isGameOver = false; 
+    let platformCount = 5
+
+
    /*  let doodlerBottomSpace = startPoint */
 
 
@@ -22,6 +26,36 @@ document.addEventListener(`DOMContentLoaded`, () => {
 
     //Assing Function To KeyCodes
 
+    //class 
+    class Platform {
+        constructor(newPlatBottom) {
+        this.bottom = newPlatBottom
+        this.left = Math.random() * 315
+        this.visual = document.createElement(`div`)
+
+        const visual = this.visual
+        visual,classList.add(`platfom`)
+        }
+    }
+
+    //Create Platform
+  /*   function createPlatforms() {
+        for (let i = 0; i < platformCount; i ++) {
+            let platGap = 600 / platformCount
+            let newPlatBottom = 100 + i * platGap
+            let newPlatform = new Platform(newPlatBottom)
+            Platform.push(newPlatform)
+            console.log(Platform)
+        }
+    } */
+
     //Start
+    function start() {
+        if(!isGameOver) {
+           createDoodler()
+           createPlatforms()
+        }
+    }
+    start()
 
 });
