63d33ba425de92ccecbfe26a6c7e5ea33beb852c
Christiano0407 <placebo0704@gmail.com>
2021-02-21 13:07:49 -0600

    platforms_moved

--- src/js/app.js
+++ src/js/app.js
@@ -4,9 +4,14 @@ document.addEventListener(`DOMContentLoaded`, () => {
     const grid = document.querySelector(`.grid`);
     const doodler = document.createElement(`div`);
     let doodlerLeftSpace = 50
+    let doodlerBottomSpace = 150
     let platforms = []
     let isGameOver = false; 
     let platformCount = 5
+    //let doodlerLeftSpace = 50
+    //let startPoint = 150
+    //let doodlerBottomSpace = startPoint
+
 
 
    /*  let doodlerBottomSpace = startPoint */
@@ -18,7 +23,7 @@ document.addEventListener(`DOMContentLoaded`, () => {
     function createDoodler() {
         grid.appendChild(doodler)
         doodler.classList.add(`doodler`)
-        doodlerLeftSpace = platforms[0].left
+        //doodlerLeftSpace = platforms[0].left
         doodler.style.left = doodlerLeftSpace + `px`
         doodler.style.bottom = doodlerBottomSpace + `px`
     }
@@ -26,34 +31,55 @@ document.addEventListener(`DOMContentLoaded`, () => {
 
     //Assing Function To KeyCodes
 
-    //class 
+    //class Platform
     class Platform {
         constructor(newPlatBottom) {
-        this.bottom = newPlatBottom
-        this.left = Math.random() * 315
-        this.visual = document.createElement(`div`)
-
-        const visual = this.visual
-        visual,classList.add(`platfom`)
+            this.left = Math.random() * 315
+            this.bottom = newPlatBottom
+            this.visual = document.createElement('div')
+      
+            const visual = this.visual
+            visual.classList.add('platform')
+            visual.style.left = this.left + 'px'
+            visual.style.bottom = this.bottom + 'px'
+            grid.appendChild(visual)
         }
     }
 
     //Create Platform
-  /*   function createPlatforms() {
+     function createPlatforms() {
         for (let i = 0; i < platformCount; i ++) {
             let platGap = 600 / platformCount
             let newPlatBottom = 100 + i * platGap
             let newPlatform = new Platform(newPlatBottom)
-            Platform.push(newPlatform)
-            console.log(Platform)
+            platforms.push(newPlatform)
+            console.log(platforms) 
         }
+    } 
+
+    //MovePlatforms
+    function movePlatforms() {
+      if(doodlerBottomSpace > 200) {
+          platforms.forEach(platform => {
+              platform.bottom -= 4
+              let visual = platform.visual
+              visual.style.bottom = platform.bottom + `px`
+          })
+      }
+    }
+
+    //Game
+  /*   function gameOver() {
+        isGameOver = true
+
     } */
 
     //Start
     function start() {
         if(!isGameOver) {
-           createDoodler()
-           createPlatforms()
+            createPlatforms()
+            createDoodler()
+            setInterval(movePlatforms, 30)
         }
     }
     start()
