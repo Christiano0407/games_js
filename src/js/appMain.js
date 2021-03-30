69ba63b18ec00f0847eba858847c5d0dca3011c8
Christiano0407 <placebo0704@gmail.com>
2021-03-29 23:14:26 -0600

    javascripts

--- src/js/appMain.js
+++ src/js/appMain.js
@@ -9,18 +9,28 @@ console.log(people);
 
 const person = ["Juan", "Ricardo", "Luisa", "Mau"];
 console.log(person);
-
-//Funciones
+/* 
 function miFunction() {
   let peso = {
-      pesa: 50,
+      grasa: 50,
   }
   console.log(peso); 
 }
 
-miFunction(); 
+miFunction();  */
 
 function activity(a, b) {
   return a + b; 
 }
 console.log(activity(5, 10)); 
+
+function saludar(saludo) {
+    console.log(`Hola ${saludo}`)
+}
+
+saludar(`Buen día`); 
+
+function imprimirEdad(edad) {
+  console.log(`Hola, mi edad es de ${edad} años`); 
+}
+imprimirEdad(29);
