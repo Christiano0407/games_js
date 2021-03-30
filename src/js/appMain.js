363dce3480c30ab52b0ca571ea7bc0405a7e2d71
Christiano0407 <placebo0704@gmail.com>
2021-03-29 22:08:45 -0600

    variables

--- src/js/appMain.js
+++ src/js/appMain.js
@@ -1,1 +1,11 @@
-console.log("Hello World!");
+var primo = "Abraham";
+console.log(primo);
+
+let people = {
+    nombre : "Luisa",
+    apellido :  "Richardson",
+};
+console.log(people); 
+
+const person = ["Juan", "Ricardo", "Luisa", "Mau"];
+console.log(person[2]);
