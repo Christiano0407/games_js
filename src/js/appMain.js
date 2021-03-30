7af3cafd825ca6b30b1a4b375f1d3e0fdf28a2ab
Christiano0407 <placebo0704@gmail.com>
2021-03-29 22:35:36 -0600

    new-js

--- src/js/appMain.js
+++ src/js/appMain.js
@@ -8,4 +8,19 @@ let people = {
 console.log(people); 
 
 const person = ["Juan", "Ricardo", "Luisa", "Mau"];
-console.log(person[2]);
+console.log(person);
+
+//Funciones
+function miFunction() {
+  let peso = {
+      pesa: 50,
+  }
+  console.log(peso); 
+}
+
+miFunction(); 
+
+function activity(a, b) {
+  return a + b; 
+}
+console.log(activity(5, 10)); 
