08562eb620c6a5cf5f86431657981015ed4c2cd8
Christiano0407 <placebo0704@gmail.com>
2021-04-13 22:26:23 -0500

    add-methods

--- src/js/array.js
+++ src/js/array.js
@@ -1,1 +1,29 @@
 console.log("Array"); 
+
+const frutas = ["Manzana", "Plátano", "Sandía", "Pera", "Naranja"]; 
+
+if(frutas.length === 5 || frutas[0] === "Manzana") {
+   console.log("Estás en lo correcto"); 
+}else {
+    console.log("Lo siento!"); 
+}; 
+
+console.log(frutas); 
+
+    //Métodos
+//lenght
+//push (Agregar al final del array)
+const masFrutas = frutas.push("Mango"); 
+console.log(frutas); 
+//pop (eliminar el último elemento del array)
+const quitar = frutas.pop("Mango"); 
+console.log(frutas.length); 
+console.log(frutas); 
+
+//unshift (Agregar un elemento al inicio del array)
+const primero = frutas.unshift("Uvas"); 
+console.log(frutas.length); 
+console.log(frutas); 
+
+//
+
