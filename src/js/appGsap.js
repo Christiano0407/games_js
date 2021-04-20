e1f6553c7c4f5a84c88e78eb1515a92fcd694074
Christiano0407 <placebo0704@gmail.com>
2021-04-20 09:23:55 -0500

    gsap-practice

---
+++ src/js/appGsap.js
@@ -0,0 +1,44 @@
+console.log("Hello World");
+
+// GSAP
+
+//SET (nos permite recoger el elemento, y editarlo).
+gsap.set(`#icon`, {
+    width:300
+})
+//TO (presenta el estado en el que va a cambiar tu diseño).
+gsap.to(`#icon`, {
+    rotation:25,
+    duration: 3, 
+    x:300, 
+    ease: `elastic`
+}); 
+
+// OBJECT
+
+//From (Muy parecido a to pero, muestra el estado dónde comienza).
+gsap.from(`#icon`, {
+  opacity: 0, 
+  duration: 2
+}); 
+
+// Tween (permite realizar animaciones a cualquier tipo).
+let Tween = gsap.from(`.mars`, {
+   duration: .5, 
+   y: -200, 
+   scale:0, 
+   stagger: 0.5,
+   delay:3
+}); 
+
+//TL (timeLine es un secuenciador para las animaciones==no se revuelvan).tl.from(`.mars`, {
+
+/* tl.from(`.mars`, { */
+/* duration: 1,  */
+/* stagger: .5 */
+/* }) */
+let tl = gsap.timeline({repeat: 1, yoyo:true}); 
+tl.from(`#mer`, {
+    duration: 3, 
+    rotate: 360
+}); 
