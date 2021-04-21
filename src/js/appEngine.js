ce24692ce0bdc5afb0af978508fb20c508acb384
Christiano0407 <placebo0704@gmail.com>
2021-04-21 09:57:55 -0500

    practice-v8

--- src/js/appEngine.js
+++ src/js/appEngine.js
@@ -1,1 +1,22 @@
 console.log("Hello World!");
+
+const hello = document.getElementById(`hello`); 
+
+if(hello === "Hola!") {
+    console.log("Yes!"); 
+}else {
+    console.log("No!");
+}
+
+/* function clicks () { */
+/*  */
+/* let  button = document.getElementById(`button`);  */
+/*  */
+/* button.addEventListener("click", { */
+/*     console: console.log("Soy un botón")  */
+/* }); */
+/*  */
+/* } */
+/*  */
+/* clicks(); */
+
